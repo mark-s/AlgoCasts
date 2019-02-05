e46e46c03bb96cec36caa1ef015f5a1c0de8c529
Mark Staff <mark@dev.haus>
2019-02-05 23:28:14 +0900

    Reverse Strings - v1

---
+++ csharp/Code/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Code
+{
+    public  class ReverseString
+    {
+        public string ReverseV1(string stringToReverse)
+        {
+            var characters = stringToReverse.ToCharArray();
+
+            Array.Reverse(characters);
+            
+            return new string(characters);
+        }
+
+
+        public string ReverseV2(string arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/Tests/ReverseStringTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Code;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ReverseStringTests
+    {
+        private Func<string, string> _reverser;
+
+        [SetUp]
+        public void Setup()
+        {
+            var rs = new ReverseString();
+            _reverser = rs.ReverseV1;
+            //_reverser = rs.ReverseV2;
+        }
+
+        [Test]
+        public void Reverse_reverses_a_string()
+        {
+            // Arrange
+            var stringToReverse = "abcd";
+
+            // Act 
+            var result = _reverser(stringToReverse);
+
+            // Assert
+            result.ShouldBe("dcba");
+        }
+
+        [Test]
+        public void Reverse_reverses_a_string_starting_with_spaces()
+        {
+            // Arrange
+            var stringToReverse = "  abcd";
+
+            // Act 
+            var result = _reverser(stringToReverse);
+
+            // Assert
+            result.ShouldBe("dcba  ");
+        }
+
+        [Test]
+        public void Reverse_reverses_a_string_apple()
+        {
+            // Arrange
+            var stringToReverse = "apple";
+
+            // Act 
+            var result = _reverser(stringToReverse);
+
+            // Assert
+            result.ShouldBe("elppa");
+        }
+
+        [Test]
+        public void Reverse_reverses_a_string_greetings()
+        {
+            // Arrange
+            var stringToReverse = "Greetings!";
+
+            // Act 
+            var result = _reverser(stringToReverse);
+
+            // Assert
+            result.ShouldBe("!sgniteerG");
+        }
+
+
+
+    }
+}
